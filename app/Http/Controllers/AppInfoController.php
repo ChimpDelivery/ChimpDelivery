35b0228f8c13ce767881ae28acdd003ee0f746de
Emre - Win <kovanciemre@gmail.com>
2022-02-23 14:09:58 +0300

    Add AppInfoRequest

--- app/Http/Controllers/AppInfoController.php
+++ app/Http/Controllers/AppInfoController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AppInfoRequest;
 use App\Models\AppInfo;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class AppInfoController extends Controller
 {
@@ -16,66 +16,46 @@ class AppInfoController extends Controller
     }
 
 
-    public function create(Request $request)
+    public function create(AppInfoRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'app_name' => 'required',
-            'app_bundle' => 'required',
-            'fb_app_id' => 'required',
-            'elephant_id' => 'required',
-            'elephant_secret' => 'required'
-        ]);
-
-        if ($validator->fails())
+        $validated = $request->validated();
+        if ($validated)
         {
+            $appInfo = new AppInfo();
+            $appInfo->app_name = $request->app_name;
+            $appInfo->app_bundle = $request->app_bundle;
+            $appInfo->fb_app_id = $request->fb_app_id;
+            $appInfo->elephant_id = $request->elephant_id;
+            $appInfo->elephant_secret = $request->elephant_secret;
+            $appInfo->save();
+
             return response()->json([
-               'status_code' => 400,
-               'message' => $validator->messages()->first()
+                'status_code' => 200
             ]);
         }
+        else
+        {
+            return response()->json([
+                'status_code' => 400
+            ]);
+        }
+    }
+
+
+    public function store(AppInfoRequest $request)
+    {
+        $validated = $request->all();
 
         $appInfo = new AppInfo();
-        $appInfo->app_name = $request->app_name;
-        $appInfo->app_bundle = $request->app_bundle;
-        $appInfo->fb_app_id = $request->fb_app_id;
-        $appInfo->elephant_id = $request->elephant_id;
-        $appInfo->elephant_secret = $request->elephant_secret;
+        $appInfo->app_name = $validated['app_name'];
+        $appInfo->app_bundle = $validated['app_bundle'];
+        $appInfo->fb_app_id = $validated['fb_app_id'];
+        $appInfo->elephant_id = $validated['elephant_id'];
+        $appInfo->elephant_secret = $validated['elephant_secret'];
         $appInfo->save();
 
         return response()->json([
             'status_code' => 200
-        ]);
-    }
-
-
-    public function store(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'app_name' => 'required',
-            'app_bundle' => 'required',
-            'fb_app_id' => 'required',
-            'elephant_id' => 'required',
-            'elephant_secret' => 'required'
-        ]);
-
-        if ($validator->fails())
-        {
-            return response()->json([
-                'status_code' => 400,
-                'message' => $validator->messages()->first()
-            ]);
-        }
-
-        $appInfo = new AppInfo();
-        $appInfo->app_name = $request->app_name;
-        $appInfo->app_bundle = $request->app_bundle;
-        $appInfo->fb_app_id = $request->fb_app_id;
-        $appInfo->elephant_id = $request->elephant_id;
-        $appInfo->elephant_secret = $request->elephant_secret;
-        $appInfo->save();
-
-        return response()->json([
-            'status' => 200
         ]);
     }
 
---
+++ app/Http/Requests/AppInfoRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AppInfoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'app_name' => 'required',
+            'app_bundle' => 'required',
+            'fb_app_id' => 'required|numeric',
+            'elephant_id' => 'required',
+            'elephant_secret' => 'required'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'app_name.required' => 'app_name field is required!',
+            'app_bundle.required' => 'app_bundle is required!',
+            'fb_app_id.required' => 'fb_app_id is required!',
+            'fb_app_id.numeric' => 'fb_app_id is incorrect!',
+            'elephant_id.required' => 'elephant_id is required!',
+            'elephant_secret.required' => 'elephant_secret is required!'
+        ];
+    }
+}
