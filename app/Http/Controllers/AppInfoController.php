edfb49e8d050ed2e72354b4a8abf88ff94de3481
Emre - Win <kovanciemre@gmail.com>
2022-02-23 14:11:35 +0300

    Update

--- app/Http/Controllers/AppInfoController.php
+++ app/Http/Controllers/AppInfoController.php
@@ -18,27 +18,19 @@ class AppInfoController extends Controller
 
     public function create(AppInfoRequest $request)
     {
-        $validated = $request->validated();
-        if ($validated)
-        {
-            $appInfo = new AppInfo();
-            $appInfo->app_name = $request->app_name;
-            $appInfo->app_bundle = $request->app_bundle;
-            $appInfo->fb_app_id = $request->fb_app_id;
-            $appInfo->elephant_id = $request->elephant_id;
-            $appInfo->elephant_secret = $request->elephant_secret;
-            $appInfo->save();
+        $validated = $request->all();
 
-            return response()->json([
-                'status_code' => 200
-            ]);
-        }
-        else
-        {
-            return response()->json([
-                'status_code' => 400
-            ]);
-        }
+        $appInfo = new AppInfo();
+        $appInfo->app_name = $validated['app_name'];
+        $appInfo->app_bundle = $validated['app_bundle'];
+        $appInfo->fb_app_id = $validated['fb_app_id'];
+        $appInfo->elephant_id = $validated['elephant_id'];
+        $appInfo->elephant_secret = $validated['elephant_secret'];
+        $appInfo->save();
+
+        return response()->json([
+            'status_code' => 200
+        ]);
     }
 
 
