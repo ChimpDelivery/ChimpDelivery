f908603a1d14f653d26456d8d369f8f8b0bc2525
Emre - Win <kovanciemre@gmail.com>
2022-02-24 03:45:01 +0300

    Add nav bar icon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,20 +22,33 @@
     @if($errors->any())
         {!! implode('', $errors->all('<div class="alert alert-danger">:message</div>')) !!}
     @endif
-    <ul class="nav justify-content-end nav-pills">
-        <li class="nav-item">
-            <a class="nav-link font-weight-bold" href="/dashboard">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link font-weight-bold active" href="/dashboard">Apps</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link font-weight-bold" href="/dashboard/add-app-info">Create App</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link font-weight-bold font-italic disabled" href="/dashboard/ci">Build App</a>
-        </li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand font-weight-bold" href="/dashboard">
+            <img src="{{ asset('Talus_icon.ico') }}" alt="..." height="36"/>
+            Dashboard
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto nav-pills">
+                <li class="nav-item {{ (request()->is('dashboard')) ? 'active' : '' }}">
+                    <a class="nav-link font-weight-bold" href="/dashboard">Apps <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item {{ (request()->is('dashboard/add-app-info')) ? 'active' : '' }}">
+                    <a class="nav-link font-weight-bold" href="/dashboard/add-app-info">Create App</a>
+                </li>
+                <li class="nav-item {{ (request()->is('dashboard')) ? 'active' : '' }}">
+                    <a class="nav-link disabled" href="#">Build App</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
 </div>
 
 <section class="page-content">
@@ -43,8 +56,8 @@
 </section>
 
 <footer class="page-footer font-small blue fixed-bottom">
-    <div class="footer-copyright text-center py-3 text-muted bg-primary">
-        <span class="text-white font-weight-bold font-italic">app count: {{ count($appInfos) }}, last update: 0 days ago</span>
+    <div class="footer-copyright text-center py-1 text-muted">
+        <span class="font-weight-bold font-italic">app count: {{ count($appInfos) }}, last update: 0 days ago</span>
     </div>
 </footer>
 </body>
