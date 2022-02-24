90e45737833cc770af7ef57b90b1d70311c209fc
Emre - Win <kovanciemre@gmail.com>
2022-02-24 08:53:08 +0300

    update page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -50,6 +49,7 @@
     </nav>
 </div>
 
+<!-- errors_begin !-->
 <div class="container-fluid">
     @if($errors->any())
         <br />
@@ -62,14 +62,17 @@
         </div>
     @endif
 </div>
+<!-- errors_end !-->
 
+<!-- contents_start !-->
 <section class="page-content">
     @yield('content')
 </section>
+<!-- contents_end !-->
 
+<!-- toast_begin !-->
 <!-- Flexbox container for aligning the toasts -->
 <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center" style="min-height: 200px;">
-
     <!-- Then put toasts within -->
     <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
         <div class="toast-header">
@@ -85,7 +88,9 @@
         </div>
     </div>
 </div>
+<!-- toast_end !-->
 
+<!-- footer_begin !-->
 <footer class="page-footer font-small blue fixed-bottom">
     <div class="footer-copyright text-center py-1 text-muted">
         <span class="font-weight-bold font-italic">app count: {{ count($appInfos) }}</span>
@@ -93,12 +98,12 @@
 </footer>
 </body>
 </html>
+<!-- footer_end !-->
 
 <script type="text/javascript">
-
     $(document).ready(function() {
         console.log("talus-toast-cookie:" + getCookie('talus-toast-cookie'));
-        
+
         if (!getCookie('talus-toast-cookie')) {
             $('.toast').toast('show');
         }
