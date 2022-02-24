65aa41296a710dbdec8c274f49df86cacde6c47d
Emre - Win <kovanciemre@gmail.com>
2022-02-24 08:58:59 +0300

    update toast

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -75,10 +75,10 @@
 <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center" style="min-height: 200px;">
     <!-- Then put toasts within -->
     <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
-        <div class="toast-header">
+        <div class="toast-header bg-primary">
             <img src="{{ asset('Talus_icon.ico') }}" class="rounded mr-2" alt="..." width="16" height="16">
-            <strong class="mr-auto">Talus Dashboard</strong>
-            <small>11 mins ago</small>
+            <strong class="mr-auto text-white">Talus Dashboard</strong>
+            <small class="text-white">7 mins ago</small>
             <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close" onclick="setToastCookie()">
                 <span aria-hidden="true">&times;</span>
             </button>
