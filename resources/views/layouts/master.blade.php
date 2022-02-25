a8916df4116b665dcde41b44f57c00046825e20c
Emre - Win <kovanciemre@gmail.com>
2022-02-25 09:57:09 +0300

    up

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>@yield('title') - Talus Dashboard</title>
+    <title>@yield('title') - {{ config('app.name') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -81,7 +81,7 @@
 <!-- toast_begin !-->
 <div class="toast-container">
     <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
-        <div class="toast-header bg-primary">
+        <div class="toast-header bg-success">
             <img src="{{ asset('Talus_icon.ico') }}" class="rounded mr-2" alt="..." width="16" height="16">
             <strong class="mr-auto text-white">Talus Dashboard</strong>
             <small class="text-white">7 mins ago</small>
