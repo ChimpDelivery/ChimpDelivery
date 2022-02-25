b2a6b8602255c6ef58945957c98015e5e26970a9
Emre - Win <kovanciemre@gmail.com>
2022-02-25 07:42:34 +0300

    update toast

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <style>
+        .toast-container { position: fixed; right: 20px; top: 20px; }
+        .toast:not(.showing):not(.show) { display: none !important; }
+    </style>
 </head>
 <body>
 
@@ -19,6 +23,7 @@
             {{ session('status') }}
         </div>
     @endif
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand font-weight-bold" href="/dashboard">
             <img src="{{ asset('Talus_icon.ico') }}" alt="..." height="36"/>
@@ -51,7 +56,7 @@
         </div>
     </nav>
 </div>
-<br />
+
 <!-- errors_begin !-->
 <div class="container-fluid">
     @if($errors->any())
@@ -74,9 +79,7 @@
 <!-- contents_end !-->
 
 <!-- toast_begin !-->
-<!-- Flexbox container for aligning the toasts -->
-<div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center" style="min-height: 200px;">
-    <!-- Then put toasts within -->
+<div class="toast-container">
     <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
         <div class="toast-header bg-primary">
             <img src="{{ asset('Talus_icon.ico') }}" class="rounded mr-2" alt="..." width="16" height="16">
