3ddd978ea719a92ba9fc7c7db25fe43c86f6801e
Emre - Win <kovanciemre@gmail.com>
2022-03-02 10:38:37 +0300

    remove unnec

--- database/migrations/2022_02_23_073935_create_app_infos_table.php
+++ database/migrations/2022_02_23_073935_create_app_infos_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('app_infos', function (Blueprint $table) {
             $table->id();
-            $table->string('app_icon')->default(null)->nullable();
+            $table->string('app_icon')->nullable();
             $table->string('app_name')->unique();
             $table->string('app_bundle')->unique();
             $table->string('appstore_id')->unique();
-            $table->string('fb_app_id')->default(null)->nullable();
-            $table->string('elephant_id')->default(null)->nullable();
-            $table->string('elephant_secret')->default(null)->nullable();
+            $table->string('fb_app_id')->nullable();
+            $table->string('elephant_id')->nullable();
+            $table->string('elephant_secret')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
