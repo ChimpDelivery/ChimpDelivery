a67e0673566bf93f61579622b282695ea602a27d
Emre Kovanci <kovanciemre@gmail.com>
2022-03-11 19:18:29 +0300

    update ux

--- resources/views/list-app-info.blade.php
+++ resources/views/list-app-info.blade.php
@@ -9,8 +9,8 @@
             <tr class="bg-dark text-light">
                 <th scope="col" class="text-center">🆔 </th>
                 <th scope="col" class="text-center">🖼️ App Icon</th>
-                <th scope="col" class="text-center">🔗 Appstore ID</th>
                 <th scope="col" class="text-center">📝 App Name</th>
+                <th scope="col" class="text-center">🔗 Appstore ID</th>
                 <th scope="col" class="text-center">🏷️ Bundle ID</th>
                 <th scope="col" class="text-center">📲 Build App</th>
                 <th scope="col" class="text-center">⚙️ Edit App</th>
@@ -23,24 +23,24 @@
                 <td class="text-center align-middle">
                     <img src="{{ asset('images/'.$appInfo->app_icon) }}" width="100px" height="100px" alt="..." class="img-thumbnail" />
                 </td>
+                <td class="text-center align-middle">{{ $appInfo->app_name }}</td>
                 <td class="text-center align-middle">
                     <a href="https://appstoreconnect.apple.com/apps/{{$appInfo->appstore_id}}/testflight">
                         {{ $appInfo->appstore_id }}
                     </a>
                 </td>
-                <td class="text-center align-middle">{{ $appInfo->app_name }}</td>
                 <td class="text-center align-middle">{{ $appInfo->app_bundle}}</td>
                 <td class="text-center align-middle">
                     <a href="dashboard/build-app/{{$appInfo->id}}">
-                        <button class="btn text-white bg-success">
-                            <i class="fa fa-cloud-upload"></i>
+                        <button class="btn text-white bg-transparent">
+                            <i style="font-size:2em;" class="fa fa-cloud-upload text-success"></i>
                         </button>
                     </a>
                 </td>
                 <td class="text-center align-middle">
                     <a href="dashboard/update-app-info/{{$appInfo->id}}">
-                        <button class="btn text-white bg-primary">
-                            <i class="fa fa-pencil"></i>
+                        <button class="btn text-white bg-transparent">
+                            <i style="font-size:2em;" class="fa fa-pencil text-primary"></i>
                         </button>
                     </a>
                 </td>
