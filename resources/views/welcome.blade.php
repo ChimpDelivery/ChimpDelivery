8395e1301379efd98b06afebd92dda672fd463b1
Emre Kovanci <kovanciemre@gmail.com>
2022-03-16 23:19:38 +0300

    up index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,46 @@
 <!DOCTYPE html>
 <html>
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Talus Studio">
     <meta name="keywords" content="Talus Studio, Mobile Games, Video Games">
     <meta name="author" content="Talus Studio">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Talus Studio</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <style>
-        .vertical-center
-		{
-		  min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
-		  min-height: 100vh; /* These two lines are counted as one :-)       */
-		  display: flex;
-		  align-items: center;
-		}
-
-        @font-face
-        {
-            src: url(bada.TTF);
-        }
-
-        h4
-        {
-              src: url(bada.TTF);
-        }
-
-        p
-        {
-            color: #696969;
+        .vertical-center {
+            min-height: 100%;
+            /* Fallback for browsers do NOT support vh unit */
+            min-height: 100vh;
+            /* These two lines are counted as one :-)       */
+            display: flex;
+            align-items: center;
         }
     </style>
 </head>
 <body>
     <div class="vertical-center">
-		<div class="container">
-			<center>
-				<h4>WHO WE ARE ?</h4>
-				<img src="talusbanner.gif" class="img-fluid" alt="Talus Studio" />
-				<p>
-				    We are a game studio founded by passionate artists and developers. Our team specializes in developing video games, music videos, commercials and animated films. We love to apply mixed-technique animation in our works. Our team based in Istanbul.
-				</p>
-				<h4>WE ARE TALUS</h4>
-				<p class="text-lowercase">info@talusstudio.com</p>
-				<a href="https://tr.linkedin.com/company/talusstudio"><img src="linkedin.png" class="img-fluid" style="width:64px; height:64px;" /></a>
-                <a href="/dashboard">Dashboard</a>
-			</center>
-		</div>
-	</div>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <div class="container text-center">
+            <h3 class="text-dark">WHO WE ARE ?</h3>
+            <img src="talusbanner.gif" class="img-fluid" alt="Talus Studio" />
+            <p>
+                We are a game studio founded by passionate artists and developers.
+                Our team specializes in developing video games, music videos, commercials and animated films.
+                We love to apply mixed-technique animation in our works.
+                Our team based in Istanbul.
+            </p>
+            <h4>WE ARE TALUS</h4>
+            <p class="text-lowercase text-muted">info@talusstudio.com</p>
+            <a href="https://tr.linkedin.com/company/talusstudio">
+                <img src="linkedin.png" alt="..." class="img-fluid" style="width:64px; height:64px;" />
+            </a>
+            <a href="/dashboard">Dashboard</a>
+        </div>
+    </div>
 </body>
 </html>
