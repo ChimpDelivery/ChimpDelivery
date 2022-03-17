e7f1939d55cb1601425d351c27b1ff179d092fb5
Emre Kovanci <kovanciemre@gmail.com>
2022-03-18 00:09:00 +0300

    activate forgot pass

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
-Route::controller(DashboardController::class)->middleware('auth')->group(function () {
+Route::controller(DashboardController::class)->middleware(['auth', 'verified'])->group(function () {
     // main route.
     Route::get('/dashboard', 'Index')->name('get_app_list');
 
