7e2e2ee56599205af8a4a72a256ba099986b5172
Emre Kovanci <kovanciemre@gmail.com>
2022-03-28 07:26:24 +0300

    view update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -146,6 +146,7 @@
             <div class="toast-header bg-success">
                 <img src="{{ asset('Talus_icon.ico') }}" class="rounded mr-2" alt="..." width="16" height="16">
                 <strong class="mr-auto text-white">Notification</strong>
+                <small class="text-white">1 mins ago</small>
                 <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
