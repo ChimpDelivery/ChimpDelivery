3fe565bf26c24e7934ac9a2f46d9dea57095b854
Emre Kovanci <kovanciemre@gmail.com>
2022-04-24 04:41:11 +0300

    update popover

--- resources/views/layouts/build-details-button.blade.php
+++ resources/views/layouts/build-details-button.blade.php
@@ -24,6 +24,7 @@
             data-toggle="popover"
             title="Build: {{ $appInfo->build_number }}"
             data-html="true"
+            data-placement="bottom"
             data-content="{{ $commitHistory }}">
         {{ $appInfo->build_status }}
     </button>
