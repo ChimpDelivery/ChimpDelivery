fa83593d1b5181222aec5d86a04bd85f39d2d6cc
Emre Kovanci <kovanciemre@gmail.com>
2022-04-25 05:01:13 +0300

    update view

--- resources/views/add-app-info-form.blade.php
+++ resources/views/add-app-info-form.blade.php
@@ -80,7 +80,7 @@
         </div>
         <div class="card-footer text-muted">
             <i class="fa fa-hashtag" aria-hidden="true"></i>
-            If you don't see the bundle id in the list, make sure there is an app using that bundle id on <a href="https://appstoreconnect.apple.com/apps">App Store Connect</a>.
+            If you don't see the app in the list, make sure there is an app on <a href="https://appstoreconnect.apple.com/apps">App Store Connect</a>.
         </div>
     </div>
 </div>
