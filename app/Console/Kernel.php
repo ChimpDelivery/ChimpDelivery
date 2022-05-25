f30d6e7ddf7cf2f57f6a3a11c343b96e8be0d90b
Emre Kovanci <kovanciemre@gmail.com>
2022-05-25 04:05:43 +0300

    update scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,9 +18,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('01:30');
-        $schedule->command('backup:monitor')->daily()->at('03:00');
+        $schedule->command('backup:clean')->daily()->at('01:00')->withoutOverlapping();
+        $schedule->command('backup:run')->daily()->at('01:30')->withoutOverlapping();
+        $schedule->command('backup:monitor')->daily()->at('03:00')->withoutOverlapping();
 
         $schedule->command(RunHealthChecksCommand::class)
             ->timezone('Europe/Istanbul')
