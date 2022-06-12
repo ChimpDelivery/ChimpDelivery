19fde687913155904c1769c2fea6d3fb5b818775
Emre Kovanci <emrekovanci@talusstudio.com>
2022-06-12 20:44:44 +0300

    remove unnec views

--- resources/views/layouts/app-info.blade.php
+++ resources/views/layouts/app-info.blade.php
@@ -1,8 +1,7 @@
 <div class="container text-center">
     <a class="text-muted" href="{{ $appInfo->git_url }}">
         @if (file_exists(public_path("images/app-icons/{$appInfo->app_icon}")) && !empty($appInfo->app_icon))
-            <img class="img-thumbnail" src="{{ asset('images/app-icons/'.$appInfo->app_icon) }}" alt="..."
-                    width="48px" height="48px" />
+            <img class="img-thumbnail" src="{{ asset('images/app-icons/'.$appInfo->app_icon) }}" alt="..."width="48px" height="48px" />
         @else
             <img class="img-thumbnail" src="{{ asset('Talus_icon.ico') }}" alt="..." style="max-width:50px;" />
         @endif
