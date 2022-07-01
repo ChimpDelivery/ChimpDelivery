d291fbaff80638e3556056f56bfe8868b92e482f
Emre Kovanci <emrekovanci@talusstudio.com>
2022-07-01 15:24:06 +0300

    update command

--- app/Console/Commands/CreateAppstoreSession.php
+++ app/Console/Commands/CreateAppstoreSession.php
@@ -10,7 +10,25 @@ class CreateAppstoreSession extends Command
     protected $signature = 'appstore:create-session';
     protected $description = 'creates FASTLANE_SESSION data';
 
-    private $twoFactorAuthBypass = 'sh /var/www/html/RubyBackend/TwoFactorBot.sh';
+    private $appStoreUser = '';
+    private $appStorePass = '';
+    private $bundleId = 'com.Talus.SlingBelt';
+    private $bundleName = 'SlingBelt';
+    private $appName = 'SlingBelt';
+    private $companyName = '';
+
+    private $twoFactorAuthBypass;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->appStoreUser = config('appstore.user_email');
+        $this->appStorePass = config('appstore.user_pass');
+        $this->companyName = config('appstore.company_name');
+
+        $this->twoFactorAuthBypass = "sh /var/www/html/RubyBackend/TwoFactorBot.sh {$this->appStoreUser} {$this->appStorePass} {$this->bundleId} {$this->bundleName} {$this->appName} {$this->companyName}";
+    }
 
     public function handle()
     {
