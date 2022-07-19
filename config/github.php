5707f91f3b1b4736aa40349fca20bb76426d0579
Emre Kovanci <emrekovanci@talusstudio.com>
2022-07-19 06:54:06 +0300

    update default value

--- config/github.php
+++ config/github.php
@@ -15,7 +15,7 @@ return [
 
     'organization_name' => env('GIT_ORGANIZATION_NAME', ''),
     'item_limit' => env('GIT_ITEM_LIMIT', 50),
-    'template_project' => env('GIT_TEMPLATE_PROJECT', 'TalusTemplate'),
+    'template_project' => env('GIT_TEMPLATE_PROJECT', 'Unity3D-Template'),
     'prototype_topic' => env('GIT_PROTOTYPE_TOPIC', ''),
 
     /*
