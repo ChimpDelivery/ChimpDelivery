744ae27e1813e98d4d494435606dcde5a21090c4
Emre Kovanci <kovanciemre@gmail.com>
2022-08-15 11:27:44 +0300

    extract view

--- resources/views/layouts/app-info-list.blade.php
+++ resources/views/layouts/app-info-list.blade.php
@@ -21,11 +21,7 @@
         @include('layouts.build-button')
     </td>
     <td class="text-center align-middle">
-        <a href="dashboard/update-app-info/?id={{ $appInfo->id }}">
-            <button class="btn text-white bg-transparent">
-                <i class="fa fa-pencil-square-o text-primary" aria-hidden="true" style="font-size:2em;"></i>
-            </button>
-        </a>
+        @include('layouts.update-button')
     </td>
 </tr>
 @endforeach
