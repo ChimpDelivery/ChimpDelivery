48ec4356b76d46a6dedea07d7b45b2fa9e304bdf
Emre Kovanci <kovanciemre@gmail.com>
2022-08-17 05:34:41 +0300

    update view

--- resources/views/workspace-settings-form.blade.php
+++ resources/views/workspace-settings-form.blade.php
@@ -1,3 +1,10 @@
+@php
+    function mask(string $str, $full = false)
+    {
+        return ($full) ? Str::mask($str, '*', 0) : Str::mask($str, '*', 2, Str::length($str) - 4);
+    }
+@endphp
+
 @extends('layouts.master')
 
 @section('title', 'Workspace Settings')
@@ -15,49 +22,52 @@
         <div class="card-body">
             <form name="workspace-settings-form" id="workspace-settings-form" method="post" action="{{ url('dashboard/workspace-settings') }}">
                 @csrf
-
-                <div class="form-group">
-                    <label for="appstore_private_key">App Store Connect - Private Key</label>
-                    <input type="text" class="form-control" name="appstore_private_key" aria-describedby="basic-addon3" value="{{ Auth::user()->workspace->appstore_private_key }}" required="">
+                <div class="alert alert-primary" role="alert">
+                    <i class="fa fa-apple" aria-hidden="true"></i>
+                    App Store Connect Settings (to create Appstore Connect API Keys: <a href="https://appstoreconnect.apple.com/access/api">Click Here</a>)
                 </div>
-
                 <div class="form-group">
-                    <label for="appstore_issuer_id">App Store Connect - Issuer ID</label>
-                    <input type="text" id="appstore_issuer_id" name="appstore_issuer_id" class="form-control" value="{{ Auth::user()->workspace->appstore_issuer_id }}" required="">
+                    <label for="appstore_private_key">Private Key</label>
+                    <input type="password" class="form-control" name="appstore_private_key" aria-describedby="basic-addon3" value="{{ Auth::user()->workspace->appstore_private_key }}" required="">
                 </div>
-
                 <div class="form-group">
-                    <label for="appstore_kid">App Store Connect - Key ID</label>
-                    <input type="text" id="appstore_kid" name="appstore_kid" class="form-control" value="{{ Auth::user()->workspace->appstore_kid }}" required="">
+                    <label for="appstore_issuer_id">Issuer ID</label>
+                    <input type="text" id="appstore_issuer_id" name="appstore_issuer_id" class="form-control" value="{{ mask(Auth::user()->workspace->appstore_issuer_id) }}" required="">
                 </div>
-
                 <div class="form-group">
-                    <label for="apple_usermail">Apple - User Mail</label>
-                    <input type="text" id="apple_usermail" name="apple_usermail" class="form-control" value="{{ Auth::user()->workspace->apple_usermail }}" required="">
+                    <label for="appstore_kid">Key ID</label>
+                    <input type="text" id="appstore_kid" name="appstore_kid" class="form-control" value="{{ mask(Auth::user()->workspace->appstore_kid) }}" required="">
                 </div>
-
-                <div class="form-group">
-                    <label for="apple_app_pass">Apple - App Specific Password</label>
-                    <input type="text" id="apple_app_pass" name="apple_app_pass" class="form-control" value="{{ Auth::user()->workspace->apple_app_pass }}" required="">
+                <div class="alert alert-primary" role="alert">
+                    <i class="fa fa-apple" aria-hidden="true"></i>
+                    Apple Settings (to create App Specific Password: <a href="https://appleid.apple.com/account/manage">Click Here</a>)
                 </div>
-
                 <div class="form-group">
-                    <label for="github_org_name">Github - Organization Name</label>
+                    <label for="apple_usermail">User Mail</label>
+                    <input type="text" id="apple_usermail" name="apple_usermail" class="form-control" value="{{ mask(Auth::user()->workspace->apple_usermail) }}" required="">
+                </div>
+                <div class="form-group">
+                    <label for="apple_app_pass">App Specific Password</label>
+                    <input type="password" id="apple_app_pass" name="apple_app_pass" class="form-control" value="{{ Auth::user()->workspace->apple_app_pass }}" required="">
+                </div>
+                <div class="alert alert-primary" role="alert">
+                    <i class="fa fa-github" aria-hidden="true"></i>
+                    Github Settings (to create Github Personal Access Token: <a href="https://github.com/settings/tokens">Click Here</a>)
+                </div>
+                <div class="form-group">
+                    <label for="github_org_name">Organization Name</label>
                     <input type="text" id="github_org_name" name="github_org_name" class="form-control" value="{{ Auth::user()->workspace->github_org_name }}" required="">
                 </div>
-
                 <div class="form-group">
-                    <label for="github_access_token">Github - Access Token</label>
-                    <input type="text" id="github_access_token" name="github_access_token" class="form-control" value="{{ Auth::user()->workspace->github_access_token }}" required="">
+                    <label for="github_access_token">Personal Access Token</label>
+                    <input type="text" id="github_access_token" name="github_access_token" class="form-control" value="{{ mask(Auth::user()->workspace->github_access_token) }}" required="">
                 </div>
-
                 <div class="form-group">
-                    <label for="github_template">Github - Template Project</label>
+                    <label for="github_template">Template Project</label>
                     <input type="text" id="github_template" name="github_template" class="form-control" value="{{ Auth::user()->workspace->github_template }}" required="">
                 </div>
-
                 <div class="form-group">
-                    <label for="github_topic">Github - Project Topic</label>
+                    <label for="github_topic">Project Topic</label>
                     <input type="text" id="github_topic" name="github_topic" class="form-control" value="{{ Auth::user()->workspace->github_topic }}" required="">
                 </div>
 
@@ -67,13 +77,6 @@
         </div>
         <div class="card-footer text-muted">
             <i class="fa fa-apple" aria-hidden="true"></i>
-            <a href="https://appstoreconnect.apple.com/access/api">Create Appstore Connect API Keys</a>
-            |
-            <i class="fa fa-apple" aria-hidden="true"></i>
-            <a href="https://appleid.apple.com/account/manage">Create App Specific Password</a>
-            |
-            <i class="fa fa-github" aria-hidden="true"></i>
-            <a href="https://github.com/settings/tokens">Create Github API Keys</a>
         </div>
     </div>
 </div>
