0fec43693432cfcdccc978cb6b17b31bab19fca0
Emre Kovanci <kovanciemre@gmail.com>
2022-08-19 01:49:49 +0300

    remove unnec env fields

--- config/appstore.php
+++ config/appstore.php
@@ -1,12 +1,6 @@
 <?php
 
 return [
-    'private_key' => env('APPSTORECONNECT_PRIVATE_KEY', ''),
-    'issuer_id' => env('APPSTORECONNECT_ISSUER_ID', ''),
-    'kid' => env('APPSTORECONNECT_KID', ''),
     'cache_duration' => env('APPSTORECONNECT_CACHE_DURATION', 5),
     'item_limit' => env('APPSTORECONNECT_ITEM_LIMIT', 50),
-    'user_email' => env('APPSTORECONNECT_USER_EMAIL', ''),
-    'user_pass' => env('APPSTORECONNECT_USER_PASS', ''),
-    'company_name' => env('APPSTORECONNECT_COMPANY_NAME', 'DemoCompany')
 ];
