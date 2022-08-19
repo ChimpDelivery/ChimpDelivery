18bdc3c21b379de85559b5ce928f8829feae1528
Emre Kovanci <kovanciemre@gmail.com>
2022-08-19 09:34:23 +0300

    update icon

--- resources/views/workspace-settings.blade.php
+++ resources/views/workspace-settings.blade.php
@@ -10,7 +10,7 @@
         <div class="card-header bg-dark text-white font-weight-bold">
             <span class="fa-stack fa-lg">
                 <i class="fa fa-square-o fa-stack-2x"></i>
-                <i class="fa fa-globe fa-stack-1x" aria-hidden="true"></i>
+                <i class="fa fa-users fa-stack-1x" aria-hidden="true"></i>
             </span>
             {{ $title }}
         </div>
