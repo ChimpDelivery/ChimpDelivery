f323127dffb3d2e0c870deda764338d25a0c0c88
Emre Kovanci <kovanciemre@gmail.com>
2022-08-19 03:58:56 +0300

    minimal update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('local'))
-        {
+        if ($this->app->environment([ 'local', 'staging' ])) {
             $this->app->register('Barryvdh\Debugbar\ServiceProvider');
         }
     }
