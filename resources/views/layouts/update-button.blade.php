8af106973949961a8380dd4fe5a93b110ae8b703
Emre Kovanci <kovanciemre@gmail.com>
2022-08-21 11:38:04 +0300

    update a href

--- resources/views/layouts/update-button.blade.php
+++ resources/views/layouts/update-button.blade.php
@@ -1,4 +1,4 @@
-<a href="dashboard/update-app-info/?id={{ $appInfo->id }}">
+<a href="dashboard/update-app-info?id={{ $appInfo->id }}">
     <button class="btn text-white bg-transparent">
         <i class="fa fa-pencil-square-o text-primary" aria-hidden="true" style="font-size:2em;"></i>
     </button>
