8b98e2ad8a9e3d70c55071bd099cb6dad27fb171
Emre Kovanci <kovanciemre@gmail.com>
2022-11-09 13:25:27 +0300

    update comments

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -24,11 +24,14 @@ use App\Actions\Jenkins\ScanOrganization;
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
+    //////////////////////////
     //// index route
+    //////////////////////////
     Route::get('/dashboard', GetIndexForm::class)->name('index');
 
+    ////////////////////////////////
     //// workspace routes
-    // create or update workspace
+    ////////////////////////////////
     Route::get('/dashboard/workspace-settings', GetWorkspaceForm::class)
         ->name('workspace_settings')
         ->middleware('permission:view workspace');
@@ -39,7 +42,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/dashboard/create-workspace-api-key', CreateWorkspaceApiKey::class)
         ->middleware('permission:update workspace');
 
-    // join workspace
     Route::get('/dashboard/workspace-join', GetJoinWorkspaceForm::class)
         ->name('workspace_join')
         ->middleware('permission:join workspace');
@@ -47,7 +49,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/dashboard/workspace-join', JoinWorkspace::class)
         ->middleware('permission:join workspace');
 
+    //////////////////////////////
     //// app info routes
+    //////////////////////////////
     Route::get('/dashboard/add-app-info', CreateAppForm::class)
         ->name('add_app_info')
         ->middleware('permission:create app');
@@ -68,15 +72,25 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->name('delete_app_info')
         ->middleware('permission:delete app');
 
+    ////////////////////////////
     //// jenkins routes
-    Route::post('/dashboard/build-app', BuildApp::class)->middleware('permission:build job');
-    Route::get('/dashboard/stop-job', StopJob::class)->middleware('permission:abort job');
-    Route::get('/dashboard/scan-repo', ScanOrganization::class)->middleware('permission:scan jobs');
+    ///////////////////////////
+    Route::post('/dashboard/build-app', BuildApp::class)
+        ->middleware('permission:build job');
 
+    Route::get('/dashboard/stop-job', StopJob::class)
+        ->middleware('permission:abort job');
+
+    Route::get('/dashboard/scan-repo', ScanOrganization::class)
+        ->middleware('permission:scan jobs');
+
+    //////////////////////////////////
     //// app store connect routes
+    /////////////////////////////////
     Route::get('/dashboard/create-bundle', CreateBundleIdForm::class)
         ->name('create_bundle')
         ->middleware('permission:create bundle');
 
-    Route::post('/dashboard/store-bundle', StoreBundleId::class);
+    Route::post('/dashboard/store-bundle', StoreBundleId::class)
+        ->middleware('permission:create bundle');
 });
