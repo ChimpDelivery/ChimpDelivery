cd6f086028612199db4566d682038991711e1d16
Emre Kovanci <kovanciemre@gmail.com>
2022-11-12 17:22:19 +0300

    update event

--- app/Events/WorkspaceChanged.php
+++ app/Events/WorkspaceChanged.php
@@ -2,33 +2,24 @@
 
 namespace App\Events;
 
-use App\Http\Requests\Workspace\StoreWorkspaceSettingsRequest;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
 
 use App\Models\Workspace;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
+use App\Http\Requests\Workspace\StoreWorkspaceSettingsRequest;
 
 class WorkspaceChanged
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $workspace;
-    public $request;
+    public Workspace $workspace;
+    public StoreWorkspaceSettingsRequest $request;
 
     public function __construct(Workspace $workspace, StoreWorkspaceSettingsRequest $request)
     {
         $this->workspace = $workspace;
         $this->request = $request;
-    }
-
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
