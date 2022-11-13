36f25ccce141b4ce3b12ac0b86dc782f79b37ed4
Emre Kovanci <kovanciemre@gmail.com>
2022-11-13 07:08:46 +0300

    sync:refactor

--- app/Actions/Api/AppStoreConnect/CreateToken.php
+++ app/Actions/Api/AppStoreConnect/CreateToken.php
@@ -4,7 +4,6 @@ namespace App\Actions\Api\AppStoreConnect;
 
 use Lorisleiva\Actions\Concerns\AsAction;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 use App\Services\AppStoreConnectService;
@@ -13,7 +12,7 @@ class CreateToken
 {
     use AsAction;
 
-    public function handle(Request $request) : JsonResponse
+    public function handle() : JsonResponse
     {
         return response()->json([
             'appstore_token' => app(AppStoreConnectService::class)
