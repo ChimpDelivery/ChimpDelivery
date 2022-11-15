9fa5bd3bf4169b2545ed61387ff442fb47bf027f
Emre Kovanci <kovanciemre@gmail.com>
2022-11-15 19:43:37 +0300

    change icon

--- resources/views/layouts/appinfo/create-privacy.blade.php
+++ resources/views/layouts/appinfo/create-privacy.blade.php
@@ -1,6 +1,9 @@
 @if(isset($appInfo))
-    <button class="btn btn-primary font-weight-bold text-white shadow" type="submit" onclick="return confirm('Are you sure?')" formaction="{{ route('create_privacy', ['id' => $appInfo->id ]) }}" formmethod="post">
-        <i class="fa fa-trash"></i>
+    <button class="btn btn-primary font-weight-bold text-white shadow" type="submit"
+            onclick="return confirm('Are you sure?')"
+            formaction="{{ route('create_privacy', ['id' => $appInfo->id]) }}"
+            formmethod="post">
+        <i class="fa fa-user-secret"></i>
         Create Privacy
     </button>
 @endif
