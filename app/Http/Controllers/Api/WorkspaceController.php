35b5b5ae409a86496a1fa32af03de07c3aa8c246
Emre Kovanci <kovanciemre@gmail.com>
2022-11-17 01:27:05 +0300

    up

--- app/Http/Controllers/Api/WorkspaceController.php
+++ app/Http/Controllers/Api/WorkspaceController.php
@@ -22,7 +22,6 @@ class WorkspaceController extends Controller
     public function Get() : Workspace
     {
         $workspace = Auth::user()->workspace;
-        $this->authorize('view', $workspace);
 
         return $workspace;
     }
@@ -31,7 +30,6 @@ class WorkspaceController extends Controller
     {
         $inviteCode = WorkspaceInviteCode::where('code', '=', $request->invite_code)->first();
 
-        $this->authorize('join', Workspace::find($inviteCode->workspace_id));
         return response()->json([
             'status' => 200
         ], Response::HTTP_ACCEPTED);
@@ -46,7 +44,6 @@ class WorkspaceController extends Controller
         // gate
         $method = $isNewUser ? 'create' : 'update';
         $action = $isNewUser ? Workspace::class : $currentWorkspace;
-        $this->authorize($method, $action);
 
         //
         $targetWorkspace = ($isNewUser) ? new Workspace() : $currentWorkspace;
