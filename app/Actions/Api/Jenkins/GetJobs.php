034f0bf4375521e5eaaf2271890f915eb724fb08
Emre Kovanci <kovanciemre@gmail.com>
2022-11-18 01:41:33 +0300

    auth

--- app/Actions/Api/Jenkins/GetJobs.php
+++ app/Actions/Api/Jenkins/GetJobs.php
@@ -5,6 +5,7 @@ namespace App\Actions\Api\Jenkins;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 use App\Services\JenkinsService;
 
@@ -18,5 +19,10 @@ class GetJobs
         $jobResponse->jenkins_data = collect($jobResponse->jenkins_data?->jobs)->pluck('name');
 
         return response()->json($jobResponse);
+    }
+
+    public function authorize() : bool
+    {
+        return !Auth::user()->isNew();
     }
 }
