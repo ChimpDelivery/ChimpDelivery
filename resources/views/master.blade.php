205e7a28fa850a5c8d3ddb9673aa2b6e8740efee
Emre Kovanci <kovanciemre@gmail.com>
2022-11-19 01:47:15 +0300

    replace bootstrap source

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -14,13 +14,13 @@
     {{-- favicon_end --}}
 
     {{-- bootstrap_begin --}}
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="{{ asset('bootstrap/bootstrap.min.js') }}"></script>
     {{-- bootstrap_end --}}
-    
+
     {{-- custom_js_start --}}
     <script src="{{ asset('js/cookie.js') }}"></script>
     {{-- custom_js_end --}}
