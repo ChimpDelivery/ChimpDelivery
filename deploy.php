9c8454755299996856be0363e443ef8ebce41fd5
Emre Kovanci <kovanciemre@gmail.com>
2022-11-20 07:34:38 +0300

    basic deploy script

---
+++ deploy.php
@@ -0,0 +1,23 @@
+<?php
+namespace Deployer;
+
+// mv ~/Downloads/DashboardSSH.pem ~/.ssh/DashboardSSH.pem
+// sudo chmod 600 ~/.ssh/mypemfile.pem (otherwise => WARNING: UNPROTECTED PRIVATE KEY FILE!)
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://emrekovanci@github.com/TalusStudio/TalusWebBackend');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', ['/var/www/html']);
+
+// Hosts
+
+host('52.30.195.144')
+    ->setRemoteUser('ubuntu')
+    ->setDeployPath('/var/www/html/TalusWebBackend')
+    ->setIdentityFile('~/.ssh/DashboardSSH.pem');
+
+after('deploy:failed', 'deploy:unlock');
