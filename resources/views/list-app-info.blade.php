cab603fc0ba7a93d9143a97cc15928cf61b71093
Emre Kovanci <kovanciemre@gmail.com>
2022-11-28 23:26:31 +0300

    up view

--- resources/views/list-app-info.blade.php
+++ resources/views/list-app-info.blade.php
@@ -18,16 +18,11 @@
                     <table class="table table-striped table-borderless table-hover">
                         <thead>
                             <tr class="text-dark text-light">
-                                <th scope="col" class="text-center col-sm-2 d-none d-sm-table-cell" data-bs-toggle="tooltip" data-bs-placement="top">
-                                </th>
-                                <th scope="col" class="text-center col-sm-3" data-bs-toggle="tooltip" data-bs-placement="top">
-                                </th>
-                                <th scope="col" class="text-center col-sm-3" data-bs-toggle="tooltip" data-bs-placement="top">
-                                </th>
-                                <th scope="col" class="text-center col-sm-2" data-bs-toggle="tooltip" data-bs-placement="top">
-                                </th>
-                                <th scope="col" class="text-center col-sm-2" data-bs-toggle="tooltip" data-bs-placement="top">
-                                </th>
+                                <th scope="col" class="d-none d-sm-table-cell" style="width: 10%;"></th>
+                                <th style="width: 30%;" scope="col"></th>
+                                <th style="width: 20%;" scope="col"></th>
+                                <th style="width: 20%;" scope="col"></th>
+                                <th style="width: 20%;" scope="col"></th>
                             </tr>
                         </thead>
                         <tbody>
