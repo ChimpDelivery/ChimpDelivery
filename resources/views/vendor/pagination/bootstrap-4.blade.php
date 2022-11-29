fbbfe50cbc6fb6a8c0e9b8a52c6b7218aa60acb9
Emre Kovanci <kovanciemre@gmail.com>
2022-11-29 04:43:03 +0300

    up pagination view

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -18,16 +18,22 @@
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    <li class="page-item disabled" aria-disabled="true">
+                        <span class="page-link">{{ $element }}</span>
+                    </li>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li class="page-item disabled" aria-current="page">
+                                <span class="page-link font-weight-bold">{{ $page }}</span>
+                            </li>
                         @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <li class="page-item">
+                                <a class="page-link font-weight-bold" href="{{ $url }}">{{ $page }}</a>
+                            </li>
                         @endif
                     @endforeach
                 @endif
