4a99411282675cdc0d2d8ef218e642d630ab97a5
Emre Kovanci <kovanciemre@gmail.com>
2022-11-29 06:34:31 +0300

    add tooltip

--- resources/views/layouts/update-button.blade.php
+++ resources/views/layouts/update-button.blade.php
@@ -1,3 +1,7 @@
-<a class="btn" href="dashboard/update-app-info?id={{ $appInfo->id }}">
+<a class="btn" href="dashboard/update-app-info?id={{ $appInfo->id }}"
+   data-toggle="tooltip"
+   data-placement="bottom"
+   title="{{ $appInfo->project_name }} ➔ Settings">
+
     <i class="fa fa-cog text-primary" aria-hidden="true" style="font-size:2em;"></i>
 </a>
