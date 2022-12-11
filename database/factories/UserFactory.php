1f8e00889c60ea01003479881bf1690c1447846e
Emre Kovanci <kovanciemre@gmail.com>
2022-12-11 17:20:01 +0300

    ci skip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => bcrypt('123456'),
+            'password' => bcrypt('123.Talus!'),
             'remember_token' => Str::random(10),
         ];
     }
