0908c29250c0b439ad1ee10fb999a50eac0d303f
Emre Kovanci <kovanciemre@gmail.com>
2022-12-12 23:50:21 +0300

    up

---
+++ resources/views/layouts/jenkins/build-status.blade.php
@@ -0,0 +1,12 @@
+@php
+    $ws = Auth::user()->workspace->githubSetting->organization_name;
+    $buildNumber = $appInfo->jenkins_data?->id;
+
+    $url = implode('/', [
+        config('jenkins.host'),
+        'buildStatus',
+        "icon?subject={$buildNumber}&job={$ws}%2F{$appInfo->project_name}%2Fmaster"
+    ]);
+@endphp
+
+<img alt="..." src="{{ $url }}" />
