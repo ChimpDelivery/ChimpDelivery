565804c7e92520c90d9133193de84ad284d4e41d
Emre Kovanci <kovanciemre@gmail.com>
2022-12-16 00:10:55 +0300

    up route uri

--- routes/api.php
+++ routes/api.php
@@ -30,8 +30,6 @@ use App\Actions\Api\Packages\UpdatePackage;
 use App\Actions\Api\S3\Provision\GetCertificate;
 use App\Actions\Api\S3\Provision\GetProvisionProfile;
 
-// use App\Actions\Api\Github\CreateRepository;
-
 ///////////////////////
 // apps
 //////////////////////
@@ -53,6 +51,8 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('appstoreconnect/get-full-info', GetFullAppInfo::class);
     Route::get('appstoreconnect/get-app-list', GetAppList::class);
     Route::get('appstoreconnect/get-build-list', GetBuildList::class);
+
+    // s3 related
     Route::get('appstoreconnect/get-cert', GetCertificate::class);
     Route::get('appstoreconnect/get-provision-profile', GetProvisionProfile::class);
 
@@ -82,7 +82,6 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     Route::get('github/get-repositories', GetRepositories::class);
     Route::get('github/get-repository', GetRepository::class);
-    // Route::post('github/create-repository', CreateRepository::class);
 });
 
 //////////////////////////////
