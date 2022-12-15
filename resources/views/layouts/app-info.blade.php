aad7aac35b217dc53b883509ceb5bf5ab4de1725
Emre Kovanci <kovanciemre@gmail.com>
2022-12-15 22:27:28 +0300

    remove logic

--- resources/views/layouts/app-info.blade.php
+++ resources/views/layouts/app-info.blade.php
@@ -1,12 +1,7 @@
 <div class="img-notify-item">
     <a class="text-muted" href="https://appstoreconnect.apple.com/apps/{{ $appInfo->appstore_id }}/testflight">
-        @if (file_exists(public_path("images/app-icons/{$appInfo->app_icon}")) && !empty($appInfo->app_icon))
-            <span class="img-notify-badge">{{ $appInfo->id }}</span>
-            <img class="img-thumbnail" src="{{ asset('images/app-icons/'.$appInfo->app_icon) }}" alt="..." style="max-width:50px;" />
-        @else
-            <span class="img-notify-badge" data-toggle="tooltip" data-placement="bottom" title="App ID">{{ $appInfo->id }}</span>
-            <img class="img-thumbnail" src="{{ asset('Talus_icon.ico') }}" alt="..." style="max-width:50px;" />
-        @endif
+        <span class="img-notify-badge" data-toggle="tooltip" data-placement="bottom" title="App ID">{{ $appInfo->id }}</span>
+        <img class="img-thumbnail" src="{{ asset('Talus_icon.ico') }}" alt="..." style="max-width:50px;" />
     </a>
 </div>
 <div class="container text-center">
