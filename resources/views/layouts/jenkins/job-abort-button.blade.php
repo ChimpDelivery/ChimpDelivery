2665c3e7bab3829274a066698620cd9abc5231ec
Emre Kovanci <kovanciemre@gmail.com>
2022-12-18 20:46:17 +0300

    up

--- resources/views/layouts/jenkins/job-abort-button.blade.php
+++ resources/views/layouts/jenkins/job-abort-button.blade.php
@@ -1,5 +1,3 @@
 <a onclick="return confirm('Are you sure?')" href="dashboard/abort-job?id={{ $appInfo->id }}&build_number={{ $appInfo->jenkins_data->id }}">
-    <button type="button" class="btn text-danger">
-        <i style="font-size:2em;" class="fa fa-ban"></i>
-    </button>
+    <i style="font-size:2em;" class="fa fa-ban text-danger"></i>
 </a>
--- resources/views/layouts/jenkins/job-build-button.blade.php
+++ resources/views/layouts/jenkins/job-build-button.blade.php
@@ -1,8 +1,8 @@
-<button id="build_button" type="button" class="btn" data-toggle="modal"
+<button id="build_button" type="button" class="btn btn-link" data-toggle="modal"
         data-target="#buildModal"
         data-project="{{ $appInfo->project_name }}"
         data-build-url="{{ route('build-app', [ 'id' => $appInfo->id ]) }}">
-    <span class="fa-stack" style="font-size: 1.5rem; text-shadow:2px 2px 2px rgb(10,10,10);">
+    <span class="fa-stack" style="font-size: 1.5rem; text-shadow:2px 2px 2px rgb(30,30,30);">
         <i class="fa fa-square fa-stack-2x"></i>
         <i class="fa fa-cloud-upload fa-stack-1x text-white"></i>
     </span>
