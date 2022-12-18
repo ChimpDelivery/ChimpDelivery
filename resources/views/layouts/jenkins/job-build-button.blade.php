00ed86c915471c4a99543f133507cf0c9494b376
Emre Kovanci <kovanciemre@gmail.com>
2022-12-18 22:10:42 +0300

    fix ui

--- resources/views/layouts/jenkins/job-build-button.blade.php
+++ resources/views/layouts/jenkins/job-build-button.blade.php
@@ -1,4 +1,4 @@
-<button id="build_button" type="button" class="btn btn-link" data-toggle="modal"
+<a id="build_button" type="button" class="btn btn-link" data-toggle="modal"
         data-target="#buildModal"
         data-project="{{ $appInfo->project_name }}"
         data-build-url="{{ route('build-app', [ 'id' => $appInfo->id ]) }}">
@@ -6,4 +6,4 @@
         <i class="fa fa-square fa-stack-2x"></i>
         <i class="fa fa-cloud-upload fa-stack-1x text-white"></i>
     </span>
-</button>
+</a>
