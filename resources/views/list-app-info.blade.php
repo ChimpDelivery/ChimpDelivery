42588a37c74ffa67026975295b6123e260f7c343
Emre Kovanci <kovanciemre@gmail.com>
2022-12-18 18:41:57 +0300

    up table

--- resources/views/list-app-info.blade.php
+++ resources/views/list-app-info.blade.php
@@ -15,7 +15,7 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-borderless table-hover">
+                    <table class="table table-striped table-borderless table-hover">
                         <thead>
                             <tr>
                                 <th style="width: 25%;" scope="col"></th>
