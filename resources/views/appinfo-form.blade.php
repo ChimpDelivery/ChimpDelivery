694a927478a7bee95fc4d4c6d00dd69d0391944e
Emre Kovanci <kovanciemre@gmail.com>
2022-12-18 21:48:35 +0300

    up

--- resources/views/appinfo-form.blade.php
+++ resources/views/appinfo-form.blade.php
@@ -130,7 +130,7 @@
             </form>
         </div>
         @php ($footerText = !isset($appInfo)
-            ? "Make sure there is an app on <a href='https://appstoreconnect.apple.com/apps'>App Store Connect</a> in <b>Prepare For Submission</b> state."
+            ? "Make sure there is an app on <a href='https://appstoreconnect.apple.com/apps'>App Store Connect</a> with <b>Prepare For Submission</b> state."
             : "<b>AppStore</b> and <b>GitHub</b> related settings can not be changed.")
         @include('layouts.dashboard.card-footer', ['text' => $footerText ])
     </div>
