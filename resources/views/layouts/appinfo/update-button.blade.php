f66ac969d86ce48a769f9cdfe537a733e4720919
Emre Kovanci <kovanciemre@gmail.com>
2022-12-18 17:01:00 +0300

    theme

--- resources/views/layouts/appinfo/update-button.blade.php
+++ resources/views/layouts/appinfo/update-button.blade.php
@@ -1,6 +1,6 @@
 <a class="btn" href="dashboard/update-app-info?id={{ $appInfo->id }}">
-    <span class="fa-stack fa-lg">
+    <span class="fa-stack" style="font-size: 1.5rem;">
         <i class="fa fa-square fa-stack-2x"></i>
-        <i class="fa fa-cog fa-stack-1x" style="color: white;"></i>
+        <i class="fa fa-cog fa-stack-1x text-white"></i>
     </span>
 </a>
