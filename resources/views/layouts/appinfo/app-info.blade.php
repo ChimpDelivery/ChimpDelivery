cad71579ef7c3880a33a4ea11006d740dce90c44
Emre Kovanci <kovanciemre@gmail.com>
2022-12-19 22:11:09 +0300

    rounded img

--- resources/views/layouts/appinfo/app-info.blade.php
+++ resources/views/layouts/appinfo/app-info.blade.php
@@ -1,9 +1,9 @@
 <div class="img-notify-item">
     <a class="text-muted" href="https://appstoreconnect.apple.com/apps/{{ $appInfo->appstore_id }}/testflight">
-        <span class="img-notify-badge" data-toggle="tooltip" data-placement="bottom" title="App ID">
+        <span class="img-notify-badge rounded" data-toggle="tooltip" data-placement="bottom" title="App ID">
             {{ $appInfo->id }}
         </span>
-        <img class="img" src="{{ \App\Actions\Api\S3\GetAppIcon::run($appInfo) }}" alt="..."
+        <img class="img rounded" src="{{ \App\Actions\Api\S3\GetAppIcon::run($appInfo) }}" alt="..."
             width="50" height="50"
         />
     </a>
