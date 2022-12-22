06c80fb7aa5bf1ecdb46ccadb4ae96a1e1fbc905
Emre Kovanci <kovanciemre@gmail.com>
2022-12-22 22:45:47 +0300

    add livewire sweet alert

---
+++ app/Http/Livewire/FbAppAds.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+
+use App\Models\AppInfo;
+use App\Actions\Api\Ftp\CreateFBAppAds;
+
+class FbAppAds extends Component
+{
+    use LivewireAlert;
+
+    public AppInfo $appInfo;
+
+    private array $response;
+
+    public function integrate()
+    {
+        $this->response = CreateFBAppAds::run(null, $this->appInfo);
+
+        $this->alert(
+            $this->response['success'] ? 'success' : 'warning',
+            $this->response['message']
+        );
+    }
+
+    public function render()
+    {
+        return <<<'HTML'
+            <div class="input-group-append">
+                <button wire:click="integrate()"
+                        class="btn alert-primary font-weight-bold"
+                        type="button"
+                        onclick="return confirm('FB App ID gonna be added in app-ads.txt, are you sure?') || event.stopImmediatePropagation()"
+                        data-toggle="tooltip"
+                        data-placement="bottom"
+                        title="App-Ads.txt integration">
+                    <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
+            </div>
+        HTML;
+    }
+}
