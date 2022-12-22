0a8cbf2346789b6456c8a400d2e4874b8e8f09c9
Emre Kovanci <kovanciemre@gmail.com>
2022-12-22 20:15:19 +0300

    add to clip

--- public/js/master.js
+++ public/js/master.js
@@ -14,3 +14,21 @@ $(document).ready(function ()
         $('#toast-talus').toast('show');
     }
 })
+
+function copyToClip(inputId)
+{
+    // Get the text field
+    let copyText = document.getElementById(inputId);
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    if (!copyText.value) { return; }
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    alert("Copied: " + copyText.value);
+}
---
+++ resources/views/layouts/dashboard/copy-clipboard-button.blade.php
@@ -0,0 +1,7 @@
+<div class="input-group-append">
+    <button class="btn alert-primary"
+            type="button"
+            onclick="copyToClip('{{ $input }}')">
+        <i class="fa fa-clipboard" aria-hidden="true"></i>
+    </button>
+</div>
