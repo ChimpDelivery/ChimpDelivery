0b4a28f680a4eaa09907b5a5e914691d29f088f6
Emre Kovanci <kovanciemre@gmail.com>
2022-12-25 00:03:40 +0300

    up navbar

--- resources/views/layouts/dashboard/navbar/workspace-user-links.blade.php
+++ resources/views/layouts/dashboard/navbar/workspace-user-links.blade.php
@@ -17,7 +17,7 @@
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle bg-transparent font-weight-bold" href="#" id="navbarDropdown"
                 role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                AppStore
+                Apple
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item bg-transparent text-dark" href="/dashboard/create-bundle">
@@ -30,7 +30,7 @@
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle bg-transparent font-weight-bold" href="#" id="navbarDropdown"
                 role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                GooglePlay
+                Google
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item bg-transparent text-dark" href="https://play.google.com/console/" target="_blank">
