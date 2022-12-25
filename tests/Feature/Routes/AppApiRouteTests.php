309f6db9da2e0c69da85038581854fb6e17de6a9
Emre Kovanci <kovanciemre@gmail.com>
2022-12-25 23:27:22 +0300

    add route test

---
+++ tests/Feature/Routes/AppApiRouteTests.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Routes;
+
+use Tests\TestCase;
+
+use App\Models\User;
+
+class AppApiRouteTests extends TestCase
+{
+    private User $user;
+
+    private array $headers = [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+    ];
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
+    /** @test */
+    public function has_get_app_route()
+    {
+        $response = $this->actingAs($this->user)
+            ->withHeaders($this->headers)
+            ->get('/api/apps/get-app');
+
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function has_create_app_route()
+    {
+        $response = $this->actingAs($this->user)
+            ->withHeaders($this->headers)
+            ->post('/api/apps/create-app');
+
+        $response->assertStatus(403);
+    }
+
+    /** @test */
+    public function has_update_app_route()
+    {
+        $response = $this->actingAs($this->user)
+            ->withHeaders($this->headers)
+            ->post('/api/apps/update-app');
+
+        $response->assertStatus(403);
+    }
+}
