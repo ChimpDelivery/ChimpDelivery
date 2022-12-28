fa5d80f4c2690c6bdbcb320276bed84791b71ed5
Emre Kovanci <kovanciemre@gmail.com>
2022-12-28 21:16:59 +0300

    up seeders

--- database/seeders/Workspace/WorkspaceInviteCodeSeeder.php
+++ database/seeders/Workspace/WorkspaceInviteCodeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders\Workspace;
 
 use Illuminate\Database\Seeder;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 use App\Models\WorkspaceInviteCode;
 
@@ -10,6 +11,14 @@ class WorkspaceInviteCodeSeeder extends Seeder
 {
     public function run()
     {
+        $output = new ConsoleOutput();
+
+        if (!app()->isLocal())
+        {
+            $output->writeln("Workspace Invite Code Seeder is not gonna run! Only local env allowed!");
+            return;
+        }
+
         WorkspaceInviteCode::factory(10)->create();
     }
 }
