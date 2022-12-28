e3b7935a6e9734e7aa5f504b06ef3bbfaa18edac
Emre Kovanci <kovanciemre@gmail.com>
2022-12-28 22:33:25 +0300

    limit seeder

--- database/seeders/User/UserSeeder.php
+++ database/seeders/User/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders\User;
 
 use Illuminate\Database\Seeder;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 use App\Models\User;
 
@@ -10,6 +11,14 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
+        $output = new ConsoleOutput();
+
+        if (!app()->isLocal())
+        {
+            $output->writeln("User seeder is not gonna run! Only local env allowed!");
+            return;
+        }
+
         foreach (range(1, 3) as $id)
         {
             User::factory()->create([
