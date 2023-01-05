8ff3258f65edcd36226c294de990226c88c45790
Emre Kovanci <kovanciemre@gmail.com>
2023-01-06 02:42:46 +0300

    extract livewire views

--- app/Http/Livewire/AppPrivacyCreator.php
+++ app/Http/Livewire/AppPrivacyCreator.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use Illuminate\Contracts\View\View;
+
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 use App\Models\AppInfo;
@@ -25,24 +27,8 @@ class AppPrivacyCreator extends Component
         );
     }
 
-    public function render()
+    public function render() : View
     {
-        return <<<'HTML'
-            <div class="input-group-append">
-                <button wire:loading.remove
-                    wire:click="integrate"
-                    class="btn btn-success rounded-right"
-                    type="button"
-                    onclick="return confirm('Privacy2 file will be created in talusstudio.com, are you sure?') || event.stopImmediatePropagation()"
-                    data-toggle="tooltip"
-                    data-placement="bottom"
-                    title="Create Privacy File">
-                    <i class="fa fa-user-secret"></i>
-                </button>
-                <div wire:loading class="btn alert-warning font-weight-bold">
-                    Wait...
-                </div>
-            </div>
-        HTML;
+        return view('livewire.app-privacy-btn');
     }
 }
--- app/Http/Livewire/CreateApiTokenView.php
+++ app/Http/Livewire/CreateApiTokenView.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use Illuminate\Contracts\View\View;
+
 class CreateApiTokenView extends Component
 {
     private string $createdToken = '**************';
@@ -13,28 +15,8 @@ class CreateApiTokenView extends Component
         $this->createdToken = auth()->user()->createApiToken();
     }
 
-    public function render()
+    public function render() : View
     {
-        return <<<'HTML'
-            <div class="input-group mb-3">
-                <label for="api_key" class="text-white font-weight-bold">
-                    Workspace API Token
-                </label>
-                <div class="input-group">
-                    <input type="text" id="api_key" name="api_key" class="form-control shadow-sm" value="{{ $this->createdToken }}" readonly>
-                    <div class="input-group-append">
-                        <button wire:click="createToken" type="button"
-                                onclick="confirm('Are you sure? Generating new API Token revokes all existing tokens of user.') || event.stopImmediatePropagation()"
-                                class="btn btn-success font-weight-bold shadow"
-                                style="width:100%;">
-                            Generate
-                        </button>
-                    </div>
-                </div>
-                <small class="form-text text-info">
-                    Workspace API Tokens are used by <b>Unity3D</b> projects to fetch app information.
-                </small>
-            </div>
-        HTML;
+        return view('livewire.user-api-token-btn');
     }
 }
