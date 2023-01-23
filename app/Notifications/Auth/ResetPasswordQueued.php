3d257feafd68524c7fda18942966e2d081528583
Emre Kovanci <kovanciemre@gmail.com>
2023-01-23 22:39:14 +0300

    up

--- app/Notifications/Auth/ResetPasswordQueued.php
+++ app/Notifications/Auth/ResetPasswordQueued.php
@@ -3,10 +3,11 @@
 namespace App\Notifications\Auth;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Auth\Notifications\ResetPassword;
 
-class ResetPasswordQueued extends ResetPassword implements ShouldQueue
+class ResetPasswordQueued extends ResetPassword implements ShouldQueue, ShouldBeEncrypted
 {
     use Queueable;
 }
--- app/Notifications/Auth/VerifyEmailQueued.php
+++ app/Notifications/Auth/VerifyEmailQueued.php
@@ -3,10 +3,11 @@
 namespace App\Notifications\Auth;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Auth\Notifications\VerifyEmail;
 
-class VerifyEmailQueued extends VerifyEmail implements ShouldQueue
+class VerifyEmailQueued extends VerifyEmail implements ShouldQueue, ShouldBeEncrypted
 {
     use Queueable;
 }
