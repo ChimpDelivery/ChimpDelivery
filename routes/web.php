faf3dd233847e834d60b9397e2994de15a11ec89
Emre Kovanci <kovanciemre@gmail.com>
2023-01-31 22:25:31 +0300

    up web route file

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,23 @@ use Illuminate\Mail\Markdown;
 
 use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
+//// main route
 Route::get('/', fn() => to_route('login'));
 
 ////
 Route::get('/promotion', fn() => view('dashboard-promotion'));
 
-////
+//// contracts
 Route::get('/privacy', fn() => Markdown::parse(File::get(resource_path('markdown/privacy.md'))))
     ->name('privacy');
 
 Route::get('/terms', fn() => Markdown::parse(File::get(resource_path('markdown/terms.md'))))
     ->name('terms');
 
-////
-Route::middleware([ 'auth', 'verified', 'can:viewHealth' ])->group(function () {
-    Route::get('health', HealthCheckResultsController::class)
-        ->name('health');
-});
+//// super-admin routes
+Route::get('health', HealthCheckResultsController::class)
+    ->middleware([ 'auth', 'verified', 'can:viewHealth' ])
+    ->name('health');
 
 require __DIR__.'/auth.php';
 require __DIR__.'/dashboard.php';
