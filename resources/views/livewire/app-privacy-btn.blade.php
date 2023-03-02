b461df725f39960f1cbebd0583fe5232dc3b76f5
Emre Kovanci <kovanciemre@gmail.com>
2023-03-02 12:00:36 +0300

    up button text

--- resources/views/livewire/app-privacy-btn.blade.php
+++ resources/views/livewire/app-privacy-btn.blade.php
@@ -1,13 +1,13 @@
 <div class="input-group-append">
     <button wire:loading.remove
             wire:click="integrate"
-            class="btn btn-success rounded-right"
+            class="btn btn-success rounded-right font-weight-bold"
             type="button"
             onclick="return confirm('Privacy2 file will be created in talusstudio.com, are you sure?') || event.stopImmediatePropagation()"
             data-toggle="tooltip"
             data-placement="bottom"
             title="Create Privacy File">
-        <i class="fa fa-user-secret"></i>
+        <i class="fa fa-user-secret"></i> Create Privacy
     </button>
     <div wire:loading class="btn alert-warning font-weight-bold">
         Wait...
