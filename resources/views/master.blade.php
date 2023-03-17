5567cbab07dae2fd507a04d658a95fe4f8887977
Emre Kovanci <kovanciemre@gmail.com>
2023-03-17 21:57:14 +0300

    add refresh meta

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title') - {{ config('app.name') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="refresh" content="1800">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
