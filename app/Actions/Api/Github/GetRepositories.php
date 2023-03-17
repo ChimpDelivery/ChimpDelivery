1ebd78bc60631c67d161c80dfa77cb838c9529d7
Emre Kovanci <kovanciemre@gmail.com>
2023-03-17 20:07:24 +0300

    extract methods

--- app/Actions/Api/Github/GetRepositories.php
+++ app/Actions/Api/Github/GetRepositories.php
@@ -6,6 +6,7 @@ use Lorisleiva\Actions\Concerns\AsAction;
 
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
 
 use App\Services\GitHubService;
 
@@ -16,42 +17,61 @@ class GetRepositories
     // http://developer.github.com/v3/repos/#list-organization-repositories
     public function handle() : JsonResponse
     {
-        $githubService = app(GitHubService::class);
-
-        $searchRepoType = $this->GetRepositoryType();
-
         // if there is no type of repository specified on workspace settings
         // just return empty bag...
-        if ($searchRepoType === 'none')
+        if ($this->GetRepositoryType() === 'none')
         {
             return response()->json([ 'response' => collect() ], Response::HTTP_OK);
         }
 
-        // response can include error_code and msg...
-        $request = $githubService->MakeGithubRequest('repo', 'org', $githubService->GetOrganizationName(), [
-            'per_page' => config('github.item_limit'),
-            'sort' => 'updated',
-            'type' => $searchRepoType,
-        ]);
+        $request = $this->GetGitHubRepositories();
 
-        $response = collect($request->getData()->response);
+        $orgProjects = collect($request->getData()->response);
+        $filteredOrgProjects = $this->FilterProjects($orgProjects);
 
-        // custom filter added. listed git projects count can be lower than GIT_ITEM_LIMIT.
-        // maybe extra organization is useful when filtering projects.
-        $filteredOrgProjects = $response->filter(function (\stdClass $project) use ($githubService) {
-            return isset($project->topics) && in_array($githubService->GetRepoTopic(), $project->topics);
+        return response()->json([
+            'response' => $this->ReorganizeProjects($filteredOrgProjects)
+        ], $request->status());
+    }
+
+    // response can include error_code and msg...
+    private function GetGitHubRepositories() : JsonResponse
+    {
+        $githubService = app(GitHubService::class);
+
+        return $githubService->MakeGithubRequest(
+            'repo',
+            'org',
+            $githubService->GetOrganizationName(),
+            [
+                'per_page' => config('github.item_limit'),
+                'sort' => 'updated',
+                'type' => $this->GetRepositoryType(),
+            ]
+        );
+    }
+
+    // custom filter added. listed git projects count can be lower than GIT_ITEM_LIMIT.
+    // maybe extra organization is useful when filtering projects.
+    private function FilterProjects(Collection $response) : Collection
+    {
+        $githubService = app(GitHubService::class);
+
+        return $response->filter(function (\stdClass $githubProject) use ($githubService) {
+            return isset($githubProject->topics) && in_array($githubService->GetRepoTopic(), $githubProject->topics);
         });
+    }
 
-        // re-organize data layout that returned from api
-        $organizationProjects = $filteredOrgProjects->values()->map(function ($project) {
+    // re-organize data layout that returned from api
+    private function ReorganizeProjects(Collection $filteredOrgProjects) : Collection
+    {
+        return $filteredOrgProjects->values()->map(function ($project) {
             return [
                 'id' => $project->id,
                 'name' => $project->name,
                 'size' => round($project->size / 1024, 2) . 'mb'
             ];
         });
-
-        return response()->json([ 'response' => $organizationProjects ], $request->status());
     }
 
     private function GetRepositoryType() : string
