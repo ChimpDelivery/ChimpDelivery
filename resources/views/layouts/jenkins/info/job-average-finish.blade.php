fbe68ca688f8526695a276dfe08aea71fd4cfc68
Emre Kovanci <kovanciemre@gmail.com>
2023-03-19 21:50:49 +0300

    refactor

--- resources/views/layouts/jenkins/info/job-average-finish.blade.php
+++ resources/views/layouts/jenkins/info/job-average-finish.blade.php
@@ -1,4 +1,4 @@
-@if ($last_build?->status == 'IN_PROGRESS')
+@if ($last_build?->status == JobStatus::IN_PROGRESS->value)
     @if (isset($last_build->estimated_time))
         Average Finish: <span class="text-primary font-weight-bold">{{ $last_build->estimated_time }}</span>
         <hr class="my-2">
