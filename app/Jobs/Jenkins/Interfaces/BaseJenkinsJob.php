26157be9930c706fe14734ad36b1caad7ad0b631
Emre Kovanci <kovanciemre@gmail.com>
2023-03-29 13:32:19 +0300

    reorder ns

--- app/Jobs/Jenkins/Interfaces/BaseJenkinsJob.php
+++ app/Jobs/Jenkins/Interfaces/BaseJenkinsJob.php
@@ -3,15 +3,15 @@
 namespace App\Jobs\Jenkins\Interfaces;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 abstract class BaseJenkinsJob implements ShouldQueue
 {
-    use Dispatchable;
-    use InteractsWithQueue;
     use Queueable;
+    use Dispatchable;
     use SerializesModels;
+    use InteractsWithQueue;
 }
