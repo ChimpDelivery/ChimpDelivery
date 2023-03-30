f069e936c5ab6c0760f8b01e75afc5ba93da841b
Emre Kovanci <kovanciemre@gmail.com>
2023-03-30 21:44:03 +0300

    up toast

--- resources/views/layouts/dashboard/toast.blade.php
+++ resources/views/layouts/dashboard/toast.blade.php
@@ -1,8 +1,8 @@
 <div id="{{ $id }}" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
-    <div class="toast-header bg-success">
+    <div class="toast-header bg-primary">
         <img src="{{ asset('default-app-icon.png') }}" class="rounded mr-2" alt="..." width="16" height="16">
         <strong class="mr-auto text-white">Notification</strong>
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close" onclick="{{ $onClose }}">
+        <button type="button" class="ml-2 mb-1 close text-white" data-dismiss="toast" aria-label="Close" onclick="{{ $onClose }}">
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
