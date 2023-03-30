9f6eca7353f5821619fcd371a2441d9b5c1182b8
Emre Kovanci <kovanciemre@gmail.com>
2023-03-30 21:51:29 +0300

    update toast icon

--- resources/views/layouts/dashboard/toast.blade.php
+++ resources/views/layouts/dashboard/toast.blade.php
@@ -1,7 +1,8 @@
 <div id="{{ $id }}" class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-autohide="false">
     <div class="toast-header bg-primary">
-        <img src="{{ asset('default-app-icon.png') }}" class="rounded mr-2" alt="..." width="16" height="16">
-        <strong class="mr-auto text-white">Notification</strong>
+        <strong class="mr-auto text-white">
+            <i class="fa fa-bell" aria-hidden="true"></i> Notification
+        </strong>
         <button type="button" class="ml-2 mb-1 close text-white" data-dismiss="toast" aria-label="Close" onclick="{{ $onClose }}">
             <span aria-hidden="true">&times;</span>
         </button>
