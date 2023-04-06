2ac889d975d5c03e9d1f0da9f7f61f93087a181d
Emre Kovanci <kovanciemre@gmail.com>
2023-04-06 18:38:31 +0300

    up

--- resources/views/log.blade.php
+++ resources/views/log.blade.php
@@ -10,14 +10,14 @@
     <div class="container py-2">
         <div class="card shadow bg-dark">
             @include('layouts.dashboard.card-header', [
-                'text' => "{$log_title}",
+                'text' => $log_title,
                 'icon' => 'fa-eye'
             ])
             <div class="card-body overflow-auto" style="max-height: 70vh;">
                 <pre class="text-white"><code class="language-csharp bg-transparent">{{ $full_log }}</code></pre>
             </div>
             @include('layouts.dashboard.card-footer', [
-                'text' => round(strlen($full_log) / 1024 / 1024, 2) . ' MB'
+                'text' => 'Log Size: ' . round(strlen($full_log) / 1024 / 1024, 2) . ' MB'
             ])
         </div>
     </div>
