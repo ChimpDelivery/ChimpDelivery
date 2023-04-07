0e0194f9a345d3b37133ded7826e55bd44e87fcc
Emre Kovanci <kovanciemre@gmail.com>
2023-04-07 17:38:44 +0300

    test

--- deploy/upload_secrets.php
+++ deploy/upload_secrets.php
@@ -4,6 +4,9 @@ namespace Deployer;
 
 desc('Upload environment file from local.');
 task('dashboard:upload:secrets', function() {
+    echo runLocally('pwd');
+    echo runLocally('test -f .env && echo env exists!');
+
     $dotenv = runLocally('cat .env');
     upload($dotenv, '{{deploy_path}}/shared/.env');
 });
