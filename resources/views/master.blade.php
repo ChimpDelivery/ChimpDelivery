0e04b6f7323b5c3964265a1402cffe040acae40e
Emre Kovanci <kovanciemre@gmail.com>
2023-12-26 11:15:52 +0300

    update bootstrap-select

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -26,8 +26,8 @@
     {{-- bootstrap_end --}}
 
     {{-- selectpicker_begin --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/css/bootstrap-select.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/js/bootstrap-select.min.js"></script>
     {{-- selectpicker_end --}}
 
     {{-- clipboard_js_start --}}
